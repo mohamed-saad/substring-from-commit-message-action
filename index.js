9ae9220846da2d75055064e0faaea65698c807da
Dr. Mohamed Saad <mohamed.saad@incorta.com>
2023-05-29 20:28:27 +0300

    refine
    Branch:

--- index.js
+++ index.js
@@ -2,14 +2,26 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
+  // inputs
   const pattern = core.getInput('pattern-to-search');
   const message = core.getInput('message');
-  console.log(`Received pattern ${pattern}!`);
-  console.log(`Received message ${message}!`);
-  console.log(`^^^^^^^^^^^^^^^^^^^^^^^^^^^`)
-  console.log(message.match(pattern));
-  console.log(`---------------------------`)
-  core.setOutput("match", "heeeeee");
+  console.log(`Received pattern: ${pattern}`);
+  console.log(`Received message: ${message}`);
+  
+  // matching
+  const regExpConstructor = new RegExp(pattern, 'gi'); 
+  let matches = message.match(pattern);
+  let result = "null";
+  if (matches) {
+    result = matches[0];
+    console.log("Found match: " + result);
+  } else {
+    console.log("Nothing found");
+  }
+  
+  // outputs
+  core.setOutput("match", result);  
 } catch (error) {
+  // failure
   core.setFailed(error.message);
 }
