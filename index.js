5b5aadb85217fb3e1750f438968281a337c3526f
Dr. Mohamed Saad <mohamed.saad@incorta.com>
2023-05-29 19:02:02 +0300

    match
    Branch:

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const github = require('@actions/github');
 
 try {
   const pattern = core.getInput('pattern-to-search');
+  const message = core.getInput('message');
   console.log(`Received pattern ${pattern}!`);
+  console.log(`Received message ${message}!`);
+  console.log(`^^^^^^^^^^^^^^^^^^^^^^^^^^^`)
+  console.log(message.match(pattern));
+  console.log(`---------------------------`)
   core.setOutput("match", "heeeeee");
 } catch (error) {
   core.setFailed(error.message);
